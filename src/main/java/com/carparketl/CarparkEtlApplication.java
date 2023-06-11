a5091e463a9a0bd8e1245017e31e93dfc464183f
Nhan Vo <vtnhanhcmus@gmail.com>
2023-06-11 12:01:42 +0800

    init source

---
+++ src/main/java/com/carparketl/CarparkEtlApplication.java
@@ -0,0 +1,13 @@
+package com.carparketl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarparkEtlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarparkEtlApplication.class, args);
+	}
+
+}
